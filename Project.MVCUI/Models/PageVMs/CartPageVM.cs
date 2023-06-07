370dff7b87fbdc5debd158c0d60304e719c93924
Casper <Casper@DESKTOP-EF6ID0V>
2023-06-07 14:27:31 +0300

    Add project files.

---
+++ Project.MVCUI/Models/PageVMs/CartPageVM.cs
@@ -0,0 +1,14 @@
+﻿using Project.MVCUI.Models.ShoppingTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Project.MVCUI;
+
+namespace Project.MVCUI.Models.PageVMs
+{
+    public class CartPageVM
+    {
+        public Cart Cart { get; set; }
+    }
+}
